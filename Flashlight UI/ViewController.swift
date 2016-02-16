4d89989e84a53328691aea24a4862b4c740a27ea
Jared Gutierrez <JaredGutierrez@airbears2-10-142-12-222.airbears2.1918.berkeley.edu>
2016-02-15 20:43:28 -0800

    Initial Commit

---
+++ Flashlight UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashlight UI
+//
+//  Created by Jared Gutierrez on 2/15/16.
+//  Copyright © 2016 Jared Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
